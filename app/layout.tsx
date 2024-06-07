515bfb21dbd51454af170bbdf8da72b67b2c3de9
furqan-qadri <furqaanqadri@gmail.com>
2024-06-08 02:58:12 +0800

    [feat] add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         {children}
         <Analytics />;
+        <SpeedInsights />
       </body>
     </html>
   );
