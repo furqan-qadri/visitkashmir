03865d46c941b04a6579457807497d5eeaff6775
furqan-qadri <furqaanqadri@gmail.com>
2024-06-07 12:55:04 +0800

    fix capitalization

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
     <>
       <div>
         <main
-          className={`flex min-h-screen flex-col gap-14 items-center  pt-24  inset-0 -z-10 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]`}
+          className={`flex min-h-screen flex-col gap-14 items-center  pt-24  inset-0 -z-10 p-4 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] text-center`}
         >
-          <h1 className="text-4xl text-white">Visit Kashmir</h1>
-          <h1 className="text-2xl text-white">Breathtaking pictures from the paradise on Earth</h1>
-          <h1 className="text-xl text-white">Coming soon!</h1>
+          <h1 className="text-3xl xl:text-5xl text-white">Visit Kashmir</h1>
+          <h1 className="text-2xl xl:text-3xl text-white">
+            Breathtaking pictures from the Paradise on Earth
+          </h1>
+          <h1 className="text-xl xl:text-3xl text-white">Coming soon!</h1>
         </main>
       </div>
     </>
