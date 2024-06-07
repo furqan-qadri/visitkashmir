f69dbd74baf9c5fd4598e8efea1d0e75b6a03a19
furqan-qadri <furqaanqadri@gmail.com>
2024-06-08 00:56:16 +0800

    [chore]move images to local for now for speed

--- app/components/SwiperSlides.jsx
+++ app/components/SwiperSlides.jsx
@@ -17,19 +17,19 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-const images = [
-  "https://images.unsplash.com/photo-1598091383021-15ddea10925d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 2
-  "https://images.unsplash.com/photo-1614056965546-42fbe24eb36c?q=80&w=2129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 3
-  "https://images.unsplash.com/photo-1566837497312-7be7830ae9b1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 4
-  "https://images.unsplash.com/photo-1614591276564-7b3e69347a48?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 5
-  "https://images.unsplash.com/photo-1634922719192-b4503cf15e07?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 6
-  "https://images.unsplash.com/photo-1639647383258-abee94a76f84?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Image 7
-  // "https://source.unsplash.com/collection/1353633/1600x907", // Image 8
-  // "https://source.unsplash.com/collection/1353633/1600x908", // Image 9
-  // "https://source.unsplash.com/collection/1353633/1600x909", // Image 10
+const localImages = [
+  "/dallake.webp",
+  "/dallake2.webp",
+  "./tarsar.webp",
+  "./pahalgam.webp",
+  "/sonmarg.webp",
 ];
 
+const remoteImages = [];
+
 export default function SwiperSlides() {
+  const allImages = [...localImages, ...remoteImages];
+
   return (
     <Swiper
       // install Swiper modules
@@ -42,7 +42,7 @@ export default function SwiperSlides() {
       onSlideChange={() => console.log("slide change")}
       className="text-white rounded-lg w-full h-full object-contain"
     >
-      {images.map((image, index) => (
+      {allImages.map((image, index) => (
         <SwiperSlide key={index} className="h-full w-full object-contain">
           <img
             src={image}
